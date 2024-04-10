51b62592fa9c652798021f66fbff40a24b9e74c4
PachaiyappanV <pachaiyappan394@gmail.com>
2024-04-10 23:23:38 +0530

    add getAllUsers controller functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
+const User = require("../models/User");
+const {
+  StatusCodes: { OK },
+} = require("http-status-codes");
+
 const getAllUsers = async (req, res) => {
-  res.send("getallusers");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(OK).json({
+    status: "success",
+    data: {
+      users,
+    },
+  });
 };
 const getSingleUser = async (req, res) => {
   res.send("getsingleuser");
