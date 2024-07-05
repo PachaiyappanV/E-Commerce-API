a09ef695bfe2ae083e9e7e5ed482684a4dea81b9
PachaiyappanV <pachaiyappan394@gmail.com>
2024-07-05 09:37:53 +0530

    feat(route): add product routes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const { connectDB } = require("./db/connect");
 //routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 //middleware
 const { notFoundMiddleware } = require("./middleware/not-found");
@@ -26,6 +27,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/product", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
