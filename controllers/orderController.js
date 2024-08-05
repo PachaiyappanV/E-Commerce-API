495671ede463695512c490d1ffa832549ddbb8cd
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-05 10:28:48 +0530

    feat(controller): initialize order controller

---
+++ controllers/orderController.js
@@ -0,0 +1,27 @@
+const createOrder = async (req, res) => {
+  res.send("create order");
+};
+
+const getAllOrders = async (req, res) => {
+  res.send("get all order");
+};
+
+const getSingleOrder = async (req, res) => {
+  res.send("get single order");
+};
+
+const getCurrentUserOrders = async (req, res) => {
+  res.send("get current orders");
+};
+
+const updateOrder = async (req, res) => {
+  res.send("update order");
+};
+
+module.exports = {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  updateOrder,
+};
